ca7705d31f2c2bc2f489c4e5d65290edae12484c
Salih Ekici <r0883622@student.thomasmore.be>
2024-01-21 21:19:36 +0100

    Seperated controls for both players

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    public bool isPlayerOne = false;
+    public bool isPlayerTwo = false;
     [SerializeField]
     private float _playerSpeed = 3.5f;
     [SerializeField]
@@ -27,8 +29,6 @@ public class Player : MonoBehaviour
     [SerializeField]
     private GameObject _shieldVisualizer;
     [SerializeField]
-    private GameObject _thrusterVisualizer;
-    [SerializeField]
     private GameObject _rightEngine;
     [SerializeField]
     private GameObject _leftEngine;
@@ -40,15 +40,18 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Script attached to: " + gameObject.name);
         _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<Spawn_Manager>();
         _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
         _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        
+
         if (_gameManager.isCoopMode == false)
         {
             transform.position = new Vector3(0, -1, 0);
         }
-        
-        
+
+
         if (_spawnManager == null)
         {
             Debug.LogError("Spawn Manager is null");
@@ -56,9 +59,9 @@ public class Player : MonoBehaviour
         if (_uiManager == null)
         {
             Debug.LogError("UI Manager is null");
-               
+
         }
-        if ( _gameManager == null)
+        if (_gameManager == null)
         {
             Debug.LogError("Game Manager is null");
         }
@@ -67,136 +70,219 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CalculateMovement();
-        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _nextFireTime)
+        if (isPlayerOne == true)
         {
-            _laserShotAudio.Play();
-            CharacterAttack();
-        } 
+            MovementPlayer1();
+            if (Input.GetKeyDown(KeyCode.Space) && Time.time > _nextFireTime && isPlayerOne == true)
+            {
+                _laserShotAudio.Play();
+                Attack();
+            }
+
+        }
+        if (isPlayerTwo == true)
+        {
+            MovementPlayer2();
+
+            if (Input.GetKeyDown(KeyCode.RightShift) && Time.time > _nextFireTime && isPlayerTwo == true)
+            {
+                _laserShotAudio.Play();
+                Attack();
+            }
+        }
+
     }
 
-    void CalculateMovement()
+    void MovementPlayer1()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         // Player movement
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
-        if (verticalInput != 0)
+
         {
-            _thrusterVisualizer.SetActive(true);
-        }
-        else
-        {
-            _thrusterVisualizer.SetActive(false);
-        }
-        if(_isSpeedActive == true)
-        {
-            transform.Translate(direction * Time.deltaTime * _playerSpeed*2);
             
-        }
-        else {
-            transform.Translate(direction * Time.deltaTime * _playerSpeed);
-        }
-        // Player bounds
-        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
-        if (transform.position.x < -9.2f)
-        {
-            transform.position = new Vector3(9.2f, transform.position.y, 0);
-        }
-        if (transform.position.x > 9.2f)
-        {
-            transform.position = new Vector3(-9.2f, transform.position.y, 0);
-        }
-    }
+            if (_isSpeedActive == true)
+            {
+                transform.Translate(direction * Time.deltaTime * _playerSpeed * 1.5f);
 
-    void CharacterAttack()
-    {
-        if (_isTripleShotActive == true)
-        {
-            Instantiate(_tripleShotPrefab, new Vector3(transform.position.x, transform.position.y + 1.03f, 0), Quaternion.identity);
-        }
-        else
-        {
-            Instantiate(_laserPrefab, new Vector3(transform.position.x, transform.position.y + 1.03f, 0), Quaternion.identity);
-        }
-        _nextFireTime = Time.time + _fireRate;
-    }
-    
-    public void Damage()
-    {
-        if(_isShieldActive == true)
-        {
-            _isShieldActive = false;
-            _shieldVisualizer.SetActive(false);
-            return;
-        }
-        else
-        {
-            _playerHealth -= 1;
-            UpdateLives(_playerHealth);
+
+            }
+            else
+            {
+                transform.Translate(direction * Time.deltaTime * _playerSpeed);
+            }
+            // Player bounds
+            transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+            if (transform.position.x < -9.2f)
+            {
+                transform.position = new Vector3(9.2f, transform.position.y, 0);
+            }
+            if (transform.position.x > 9.2f)
+            {
+                transform.position = new Vector3(-9.2f, transform.position.y, 0);
+            }
+
 
         }
-        if(_playerHealth == 2) { 
-            _rightEngine.SetActive(true);
-        }
-        if (_playerHealth == 1)
+    }
+        void MovementPlayer2()
         {
-            _leftEngine.SetActive(true);
+
+            KeyCode up = KeyCode.I;
+            KeyCode right = KeyCode.L;
+            KeyCode down = KeyCode.K;
+            KeyCode left = KeyCode.J;
+
+
+            if (_isSpeedActive == true)
+            {
+                if (Input.GetKey(up))
+                {
+                    transform.Translate(Vector3.up * _playerSpeed * 1.5f * Time.deltaTime);
+                }
+                if (Input.GetKey(down))
+                {
+                    transform.Translate(Vector3.down * _playerSpeed * 1.5f * Time.deltaTime);
+                }
+                if (Input.GetKey(left))
+                {
+                    transform.Translate(Vector3.left * _playerSpeed * 1.5f * Time.deltaTime);
+
+                }
+                if (Input.GetKey(right))
+                {
+                    transform.Translate(Vector3.right * _playerSpeed * 1.5f * Time.deltaTime);
+                }
+
+            }
+            else
+            {
+                if (Input.GetKey(up))
+                {
+                    transform.Translate(Vector3.up * _playerSpeed * Time.deltaTime);
+                }
+                if (Input.GetKey(down))
+                {
+                    transform.Translate(Vector3.down * _playerSpeed * Time.deltaTime);
+                }
+                if (Input.GetKey(left))
+                {
+                    transform.Translate(Vector3.left * _playerSpeed * Time.deltaTime);
+
+                }
+                if (Input.GetKey(right))
+                {
+                    transform.Translate(Vector3.right * _playerSpeed * Time.deltaTime);
+                }
+
+                // Player bounds
+                transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+                if (transform.position.x < -9.2f)
+                {
+                    transform.position = new Vector3(9.2f, transform.position.y, 0);
+                }
+                if (transform.position.x > 9.2f)
+                {
+                    transform.position = new Vector3(-9.2f, transform.position.y, 0);
+                }
+            }
+
         }
-        if (_playerHealth < 1) {
-            _spawnManager.OnPlayerDeath();
-            Destroy(this.gameObject);
-            
+
+        void Attack()
+        {
+            if (_isTripleShotActive == true)
+            {
+                Instantiate(_tripleShotPrefab, new Vector3(transform.position.x, transform.position.y + 1.03f, 0), Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(_laserPrefab, new Vector3(transform.position.x, transform.position.y + 1.03f, 0), Quaternion.identity);
+            }
+            _nextFireTime = Time.time + _fireRate;
         }
-        Debug.Log(_playerHealth);
-    }
-
-    public void TripleShotActive()
-    {
-        _isTripleShotActive = true;
-        StartCoroutine(StartTripleShotCoroutine());
-    }
-
-    IEnumerator StartTripleShotCoroutine()
-    {
-        yield return new WaitForSeconds(5);
-        _isTripleShotActive = false;
-    }
-
-    public void SpeedActive()
-    {
-        _isSpeedActive = true;
-        StartCoroutine(StartSpeedPowerupCoroutine());
         
-    }
-    IEnumerator StartSpeedPowerupCoroutine()
-    {
-        yield return new WaitForSeconds(5);
-        _isSpeedActive = false;
-    }
-    public void ShieldActive()
-    {
-        _isShieldActive = true;
-        _shieldVisualizer.SetActive(true);
-    }
 
-    public void AddScore(int points)
-    {
-        _score += points;
-        _uiManager.UpdateScore(_score);
-    }
+        public void Damage()
+        {
+            if (_isShieldActive == true)
+            {
+                _isShieldActive = false;
+                _shieldVisualizer.SetActive(false);
+                return;
+            }
+            else
+            {
+                _playerHealth -= 1;
+                UpdateLives(_playerHealth);
 
-    public void UpdateLives(int currentLives)
-    {
-        _uiManager.UpdateLives(currentLives);
-    }
+            }
+            if (_playerHealth == 2)
+            {
+                _rightEngine.SetActive(true);
+            }
+            if (_playerHealth == 1)
+            {
+                _leftEngine.SetActive(true);
+            }
+            if (_playerHealth < 1)
+            {
+                _spawnManager.OnPlayerDeath();
+                Destroy(this.gameObject);
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Enemy Laser") {
-            Destroy(collision.gameObject);
-            Damage();
+            }
+            Debug.Log(_playerHealth);
         }
-    }
+
+        public void TripleShotActive()
+        {
+            _isTripleShotActive = true;
+            StartCoroutine(StartTripleShotCoroutine());
+        }
+
+        IEnumerator StartTripleShotCoroutine()
+        {
+            yield return new WaitForSeconds(5);
+            _isTripleShotActive = false;
+        }
+
+        public void SpeedActive()
+        {
+            _isSpeedActive = true;
+            StartCoroutine(StartSpeedPowerupCoroutine());
+
+        }
+        IEnumerator StartSpeedPowerupCoroutine()
+        {
+            yield return new WaitForSeconds(5);
+            _isSpeedActive = false;
+        }
+       public  void ShieldActive()
+        {
+            _isShieldActive = true;
+            _shieldVisualizer.SetActive(true);
+        }
+
+        public void AddScore(int points)
+        {
+            _score += points;
+            _uiManager.UpdateScore(_score);
+        }
+
+        public void UpdateLives(int currentLives)
+        {
+            _uiManager.UpdateLives(currentLives);
+        }
+
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.tag == "Enemy Laser")
+            {
+                Destroy(collision.gameObject);
+                Damage();
+            }
+        }
+    
 }
